15cd9eba4a63f3b36863daa12ebaded1c49b6464
marlonferreira <marlon.daniel.df@gmail.com>
2023-03-07 13:13:23 +0000

    Struct WorldInfo para guardar a informação recebida pelo xml. Função printWorldInfo para motivos de debug. Algumas alterações na lerXML. A função retorna a struct WorldInfo. Removidos warning relacionados com coisas deprecated.

--- Engine/engine.cpp
+++ Engine/engine.cpp
@@ -1,4 +1,5 @@
 #include "engine.hpp"
+#include <unistd.h>
 
 int window_w = 800;
 int window_h = 800;
@@ -9,6 +10,58 @@ float camup[3];
 float camproj[3];
 
 vector<string> ms;
+
+void printWorldInfo(const WorldInfo& world_info) {
+    std::cout << "Window width: " << world_info.window_w << "\nWindow height: " << world_info.window_h << "\n\n";
+    std::cout << "Position: " << world_info.campos[0] << " " << world_info.campos[1] << " " << world_info.campos[2] << "\n";
+    std::cout << "LookAt: " << world_info.camlook[0] << " " << world_info.camlook[1] << " " << world_info.camlook[2] << "\n";
+    std::cout << "Up: " << world_info.camup[0] << " " << world_info.camup[1] << " " << world_info.camup[2] << "\n";
+    std::cout << "Projection: " << world_info.camproj[0] << " " << world_info.camproj[1] << " " << world_info.camproj[2] << "\n\n";
+
+    std::cout << "Models:\n";
+    for (const auto& model : world_info.models) {
+        std::cout << model << "\n";
+    }
+}
+
+WorldInfo lerXML(char* file) {
+    WorldInfo world_info;
+
+    xml_document f;
+    xml_parse_result result = f.load_file(file, parse_default, encoding_auto);
+
+    xml_node window = f.child("world").child("window");
+    world_info.window_w = window.attribute("width").as_int();
+    world_info.window_h = window.attribute("height").as_int();
+
+    xml_node cam = f.child("world").child("camera");
+    xml_node pos = cam.child("position");
+    world_info.campos[0] = pos.attribute("x").as_float();
+    world_info.campos[1] = pos.attribute("y").as_float();
+    world_info.campos[2] = pos.attribute("z").as_float();
+
+    xml_node look = cam.child("lookAt");
+    world_info.camlook[0] = look.attribute("x").as_float();
+    world_info.camlook[1] = look.attribute("y").as_float();
+    world_info.camlook[2] = look.attribute("z").as_float();
+
+    xml_node up = cam.child("up");
+    world_info.camup[0] = up.attribute("x").as_float();
+    world_info.camup[1] = up.attribute("y").as_float();
+    world_info.camup[2] = up.attribute("z").as_float();
+
+    xml_node proj = cam.child("projection");
+    world_info.camproj[0] = proj.attribute("fov").as_float();
+    world_info.camproj[1] = proj.attribute("near").as_float();
+    world_info.camproj[2] = proj.attribute("far").as_float();
+
+    xml_node models = f.child("world").child("group").child("models");
+    for (xml_node model = models.child("model"); model; model = model.next_sibling("model")) {
+        world_info.models.push_back(model.attribute("file").value());
+    }
+
+    return world_info;
+}
 
 void changeSize(int w, int h) {
 	// prevent a divide by zero, when window is too short
@@ -49,7 +102,23 @@ void renderScene(void) {
 }
 
 int main(int argc, char** argv) {
-	lerXML("C:/Users/Utilizador/Desktop/CompGrafica/Trabalho/Engine/test_1_5.xml");
+    char str[] = "../data/teste.xml";
+    
+    WorldInfo world_info = lerXML(str);
+
+    std::cout << "Window width: " << world_info.window_w << "\nWindow height: " << world_info.window_h << "\n\n";
+    std::cout << "Position: " << world_info.campos[0] << " " << world_info.campos[1] << " " << world_info.campos[2] << "\n";
+    std::cout << "LookAt: " << world_info.camlook[0] << " " << world_info.camlook[1] << " " << world_info.camlook[2] << "\n";
+    std::cout << "Up: " << world_info.camup[0] << " " << world_info.camup[1] << " " << world_info.camup[2] << "\n";
+    std::cout << "Projection: " << world_info.camproj[0] << " " << world_info.camproj[1] << " " << world_info.camproj[2] << "\n\n";
+
+    std::cout << "Models:\n";
+    for (const auto& model : world_info.models) {
+        std::cout << model << "\n";
+    }
+    
+    printWorldInfo(world_info);
+    
 	// GLUT init
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
@@ -73,46 +142,4 @@ int main(int argc, char** argv) {
 	return 1;
 }
 
-void lerXML(char* file) {
-	xml_document f;
-	xml_parse_result result = f.load_file(file, parse_default, encoding_auto);
 
-	xml_node window = f.child("world").child("window");
-	window_w = window.attribute("width").as_int();
-	window_h = window.attribute("height").as_int();
-	std::cout << "Window width: " << window_w << "\nWindow height: " << window_h << "\n\n";
-
-	xml_node cam = f.child("world").child("camera");
-	xml_node pos = cam.child("position");
-	campos[0] = pos.attribute("x").as_float();
-	campos[1] = pos.attribute("y").as_float();
-	campos[2] = pos.attribute("z").as_float();
-	std::cout << "Position: " << campos[0] << " " << campos[1] << " " << campos[2] << "\n";
-
-	xml_node look = cam.child("lookAt");
-	camlook[0] = look.attribute("x").as_float();
-	camlook[1] = look.attribute("y").as_float();
-	camlook[2] = look.attribute("z").as_float();
-	std::cout << "LookAt: " << camlook[0] << " " << camlook[1] << " " << camlook[2] << "\n";
-
-	xml_node up = cam.child("up");
-	camup[0] = up.attribute("x").as_float();
-	camup[1] = up.attribute("y").as_float();
-	camup[2] = up.attribute("z").as_float();
-	std::cout << "Up: " << camup[0] << " " << camup[1] << " " << camup[2] << "\n";
-
-	xml_node proj = cam.child("projection");
-	camproj[0] = proj.attribute("fov").as_float();
-	camproj[1] = proj.attribute("near").as_float();
-	camproj[2] = proj.attribute("far").as_float();
-	std::cout << "Projection: " << camproj[0] << " " << camproj[1] << " " << camproj[2] << "\n\n";
-
-	xml_node models = f.child("world").child("group").child("models");
-	int i = 0;
-	for (xml_node model = models.child("model"); model; model = model.next_sibling("model")) {
-		i++;
-		ms.resize(i);
-		ms[i - 1] = model.attribute("file").value();
-		std::cout << "Model: " << ms[i-1] << "\n";
-	}
-}
--- Engine/engine.hpp
+++ Engine/engine.hpp
@@ -1,5 +1,8 @@
 #ifdef __APPLE__
-#include <GLUT/glut.h>
+// Defined before OpenGL and GLUT includes to avoid deprecation messages
+#define GL_SILENCE_DEPRECATION
+#include <GL/glut.h>
+
 #else
 #include <GL/glut.h>
 #endif
@@ -17,7 +20,17 @@
 using namespace std;
 using namespace pugi;
 
+struct WorldInfo {
+    int window_w;
+    int window_h;
+    float campos[3];
+    float camlook[3];
+    float camup[3];
+    float camproj[3];
+    std::vector<std::string> models;
+};
 
-void lerXML(char* file);
+WorldInfo lerXML(char* file);
 void changeSize(int w, int h);
 void renderScene(void);
+void printWorldInfo(const WorldInfo& world_info);
