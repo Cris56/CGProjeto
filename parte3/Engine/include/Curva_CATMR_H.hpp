392b6f8d6476de765ca24ff5e2013bca9643d5c4
Jorge Silva <63512109+JorgeDPSilva@users.noreply.github.com>
2023-05-05 23:03:38 +0100

    head curves update

--- parte3/Engine/include/Curva_CATMR_H.hpp
+++ parte3/Engine/include/Curva_CATMR_H.hpp
@@ -2,9 +2,9 @@
 #ifndef Curva_CATMR_H
 #define Curva_CATMR_H
 
-void mkMatrix(float* x, float* y, float* z, float* m);
-void cross(float* a, float* b, float* res);
+void makeMatrix(float* x, float* y, float* z, float* m);
+void ProdVet(float* a, float* b, float* res);
 void normaliza(float* a);
-void getCMRPoint(float t, float* p0, float* p1, float* p2, float* p3, float* pos, float* deriv);
+void CMRPoint(float t, float* p0, float* p1, float* p2, float* p3, float* pos, float* deriv);
 
 #endif
