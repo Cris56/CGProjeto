8749a3efb0c2636899fdb6d87ff9142913ce7798
Jorge Silva <63512109+JorgeDPSilva@users.noreply.github.com>
2023-05-05 23:04:59 +0100

    new update

--- parte3/Engine/include/Curva_CATMR_H.hpp
+++ parte3/Engine/include/Curva_CATMR_H.hpp
@@ -4,6 +4,7 @@
 
 void makeMatrix(float* x, float* y, float* z, float* m);
 void ProdVet(float* a, float* b, float* res);
+void multMatrixVector(const float* m, const float* v, float* res);
 void normaliza(float* a);
 void CMRPoint(float t, float* p0, float* p1, float* p2, float* p3, float* pos, float* deriv);
 
